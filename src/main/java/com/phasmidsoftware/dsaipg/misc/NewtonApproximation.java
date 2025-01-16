c5777aa8bb6e015e06a41c2d23c5fe3e4220da5e
Dany <nohonetsigha.d@northeastern.edu>
2025-01-16 10:54:07 -0500

    solved assignment 0 for sec(x)=x

--- src/main/java/com/phasmidsoftware/dsaipg/misc/NewtonApproximation.java
+++ src/main/java/com/phasmidsoftware/dsaipg/misc/NewtonApproximation.java
@@ -9,13 +9,18 @@ class NewtonApproximation {
         // Newton's Approximation to solve cos(x) = x
         double x = 1.0;
         int left = 200;
+//        int left = 50000;
         for (; left > 0; left--) {
-            final double y = Math.cos(x) - x;
+//            final double y = Math.cos(x) - x;
+            final double y = ( 1 / Math.cos(x) ) - x;
+//            System.out.println(y);
             if (Math.abs(y) < 1E-7) {
-                System.out.println("the solution to cos(x)=x is: " + x);
+//                System.out.println("the solution to cos(x)=x is: " + x);
+                System.out.println("the solution to sex(x)=x is: " + x);
                 System.exit(0);
             }
-            x = x + y / (Math.sin(x) + 1);
+//            x = x + y / (Math.sin(x) + 1);
+            x = x - ( y / ( ( 1 / Math.cos(x) ) * Math.tan(x) - 1 ) );
         }
     }
 }
